2631035adfec281096ad7a6328455950017a6387
supernan1014 <574746215@qq.com>
2020-12-31 10:44:23 +0800

    uip3

--- source/conf.py
+++ source/conf.py
@@ -1,16 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import os
 import sys
+sys.path.insert(0, os.path.abspath('.'))
+os.system('pip install yummy-sphinx-theme')
 
-sys.path.append(os.path.join(os.environ["SAMPLE_DOCS_LOCATION"], "demo"))
-print("", sys.path[-1], "", sep="\n" + "-" * 80 + "\n")
+
 
 # -- Project information -----------------------------------------------------
 
-project = "Sphinx Themes Sample"
-copyright = "2020, Pradyun Gedam"
-author = "Pradyun Gedam"
+project = 'TUST'
+copyright = '2020, Zhu'
+author = 'Zhu'
 
-# -- Extensions --------------------------------------------------------------
+# The full version, including alpha/beta/rc tags
+release = '1.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.autodoc",
@@ -18,11 +39,32 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+master_doc = 'index'
+
+
 # -- Options for HTML output -------------------------------------------------
 
-html_title = project
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+import yummy-sphinx-theme
+html_theme = 'yummy_sphinx_theme'
 
-# NOTE: All the lines are after this are the theme-specific ones. These are
-#       written as part of the site generation pipeline for this project.
-# !! MARKER !!
-html_theme = "yummy_sphinx_theme"
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+html_search_language = 'zh'
